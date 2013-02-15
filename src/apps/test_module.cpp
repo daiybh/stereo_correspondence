0da788843343c664fa3e69bc57871ed0c8f62480
Zdenek Travnicek <travnicek@iim.cz>
2013-02-15 18:31:25 +0100

    Changed test_module to support windows modules

--- src/apps/test_module.cpp
+++ src/apps/test_module.cpp
@@ -6,8 +6,13 @@
  */
 
 #include <iostream>
-#include <dlfcn.h>
 #include <boost/cstdint.hpp>
+#if defined __linux__
+#include <dlfcn.h>
+#elif defined _WIN32
+#include <windows.h>
+#endif
+
 
 int main(int argc, char** argv)
 {
@@ -16,14 +21,29 @@ int main(int argc, char** argv)
 		std::cerr << "Usage " << argv[0] <<" <path-to-module>\n";
 		return 1;
 	}
+#if defined __linux__
 	void *handle=dlopen(argv[1],RTLD_NOW|RTLD_GLOBAL);
+#elif defined _WIN32
+	HINSTANCE handle = LoadLibrary(argv[1]);
+#endif
 	if (!handle) {
-		std::cerr << "Failed to open file " << argv[1] <<": "<< dlerror() <<"\n";
+		std::cerr << "Failed to open file " << argv[1] 
+#if defined __linux__
+			<<": " << dlerror() 
+#endif
+			<<"\n";
 		return 1;
 	}
 	// The ugly cast to uintptr_t is here for the sole purpose of silencing g++ warnings.
-	const char * (*get_name)(void) = reinterpret_cast<const char * (*)(void)>(reinterpret_cast<uintptr_t>(dlsym(handle,"yuri_module_get_name")));
-	void (*register_module)(void) = reinterpret_cast<void (*)(void)>(reinterpret_cast<uintptr_t>(dlsym(handle,"yuri_module_register")));
+	typedef const char * (*get_name_t)(void);
+	typedef void (*register_module_t)(void);
+#if defined __linux
+	get_name_t get_name = reinterpret_cast<get_name_t>(reinterpret_cast<uintptr_t>(dlsym(handle,"yuri_module_get_name")));
+	register_module_t register_module = reinterpret_cast<register_module_t>(reinterpret_cast<uintptr_t>(dlsym(handle,"yuri_module_register")));
+#elif defined _WIN32
+	get_name_t get_name = reinterpret_cast<get_name_t>(GetProcAddress(handle,"yuri_module_get_name"));
+	register_module_t register_module = reinterpret_cast<register_module_t>(GetProcAddress(handle,"yuri_module_register"));
+#endif
 	bool valid = true;
 	if (!get_name) {
 		std::cerr << "Module doesn't export yuri_module_get_name\n";
@@ -42,7 +62,11 @@ int main(int argc, char** argv)
 			std::cerr << "Module " << name << " seem valid\n";
 		}
 	}
+#if defined __linux
 	dlclose(handle);
+#elif defined _WIN32
+	FreeLibrary(handle);
+#endif
 	if (!valid) {
 		return 1;
 	}
