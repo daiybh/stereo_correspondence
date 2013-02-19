084f65df532abbe9f1044bbe06ef2ae06013e29a
Zdenek Travnicek <travnicek@iim.cz>
2013-02-19 18:41:15 +0100

    Added module Temperature

---
+++ src/modules/temperature/Temperature.h
@@ -0,0 +1,33 @@
+/*!
+ * @file 		Temperature.h
+ * @author 		Zdenek Travnicek
+ * @date 		11.2.2013
+ * @date		16.2.2013
+ * @copyright	Institute of Intermedia, 2013
+ * 				Distributed under GNU Public License 3.0
+ *
+ */
+
+#ifndef Temperature_H_
+#define Temperature_H_
+
+#include "yuri/io/BasicIOThread.h"
+
+namespace yuri {
+namespace temperature {
+
+class Temperature: public yuri::io::BasicIOThread
+{
+public:
+	IO_THREAD_GENERATOR_DECLARATION
+	static shared_ptr<config::Parameters> configure();
+	virtual ~Temperature();
+private:
+	Temperature(log::Log &log_,io::pThreadBase parent,config::Parameters &parameters);
+	virtual bool step();
+	virtual bool set_param(config::Parameter& param);
+};
+
+} /* namespace dummy_module */
+} /* namespace yuri */
+#endif /* Temperature_H_ */
