f231a960407fe0ccd1235e9a1e097f032059666a
Zdenek Travnicek <travnicek@iim.cz>
2013-07-15 00:28:03 +0200

    Added BasicEventConsumer

---
+++ src/yuri/event/BasicEventConsumer.cpp
@@ -0,0 +1,66 @@
+/*!
+ * @file 		BasicEventConsumer.cpp
+ * @author 		Zdenek Travnicek <travnicek@iim.cz>
+ * @date 		04.07.2013
+ * @copyright	Institute of Intermedia, 2013
+ * 				Distributed under GNU Public License 3.0
+ *
+ */
+
+#include "BasicEventConsumer.h"
+namespace yuri {
+namespace event {
+
+BasicEventConsumer::~BasicEventConsumer()
+{
+
+}
+
+
+bool BasicEventConsumer::receive_event(const std::string& event_name, const pBasicEvent& event)
+{
+	lock _(incomming_mutex_);
+	return do_receive_event(event_name, event);
+}
+
+bool BasicEventConsumer::do_receive_event(const std::string& event_name, const pBasicEvent& event)
+{
+	event_record_t rec{event_name, event};
+	incomming_events_.push_back(rec);
+	while (incomming_events_.size() > incomming_max_size_) {
+		incomming_events_.pop_front();
+	}
+	return true;
+}
+bool BasicEventConsumer::process_events(ssize_t max_count)
+{
+	lock _(incomming_mutex_);
+	return do_process_events(max_count);
+}
+
+bool BasicEventConsumer::do_process_events(ssize_t max_count)
+{
+	while (incomming_events_.size() && max_count!=0) {
+		auto rec = incomming_events_.front();
+		incomming_events_.pop_front();
+		try {
+			//if (!do_process_event(rec.first, rec.second)) return false;
+			do_process_event(rec.first, rec.second);
+		}
+		catch (std::runtime_error& e) {
+			do_report_consumer_error(e.what());
+		}
+	}
+	return true;
+}
+size_t	BasicEventConsumer::pending_events() const
+{
+	lock _(incomming_mutex_);
+	return incomming_events_.size();
+}
+
+
+}
+}
+
+
---
+++ src/yuri/event/BasicEventConsumer.h
@@ -0,0 +1,47 @@
+/*!
+ * @file 		BasicEventConsumer.h
+ * @author 		Zdenek Travnicek <travnicek@iim.cz>
+ * @date 		03.07.2013
+ * @copyright	Institute of Intermedia, 2013
+ * 				Distributed under GNU Public License 3.0
+ *
+ */
+
+#ifndef BASICEVENTCONSUMER_H_
+#define BASICEVENTCONSUMER_H_
+#include "yuri/event/BasicEvent.h"
+#include <deque>
+namespace yuri {
+namespace event {
+typedef std::pair<std::string, const pBasicEvent>
+								event_record_t;
+class BasicEventConsumer;
+typedef shared_ptr<BasicEventConsumer>
+								pBasicEventConsumer;
+typedef weak_ptr<BasicEventConsumer>
+								pwBasicEventConsumer;
+
+class BasicEventConsumer {
+public:
+								BasicEventConsumer() = default;
+	virtual						~BasicEventConsumer();
+	bool 						receive_event(const std::string& event_name, const pBasicEvent& event);
+protected:
+	event_record_t 				fetch_event();
+	bool 						process_events(ssize_t max_count = -1);
+	size_t						pending_events() const;
+private:
+	bool 						do_receive_event(const std::string& event_name, const pBasicEvent& event);
+	bool 						do_process_events(ssize_t max_count);
+	virtual bool 				do_process_event(const std::string& event_name, const pBasicEvent& event) = 0;
+	virtual void				do_report_consumer_error(const std::string&) {}
+	std::deque<event_record_t> 	incomming_events_;
+	mutable mutex				incomming_mutex_;
+	size_t						incomming_max_size_ = 1024;
+};
+
+}
+}
+
+
+#endif /* BASICEVENTCONSUMER_H_ */
