08ae63ef43f90d0a2a49f7991c630db3fc8b2296
Zdenek Travnicek <travnicek@iim.cz>
2013-09-07 15:55:58 +0200

    Added make_unique and make_unique_uninitialized functions

---
+++ src/yuri/core/make_unique.h
@@ -0,0 +1,57 @@
+/*
+ * make_unique.h
+ *
+ *  Created on: 6.9.2013
+ *      Author: neneko
+ */
+
+#ifndef MAKE_UNIQUE_H_
+#define MAKE_UNIQUE_H_
+
+/*
+ * std::make_unique, by. S. T. Lavalej, from N3656
+ * (with modified identifiers)
+ */
+
+#include <memory>
+#include <type_traits>
+#include <utility>
+namespace yuri {
+template<class T> struct unique_if {
+      typedef std::unique_ptr<T> single_object;
+  };
+
+  template<class T> struct unique_if<T[]> {
+      typedef std::unique_ptr<T[]> unknown_bound;
+  };
+
+  template<class T, size_t N> struct unique_if<T[N]> {
+      typedef void known_bound;
+  };
+
+  template<class T, class... Args>
+      typename unique_if<T>::single_object
+      make_unique(Args&&... args) {
+          return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+      }
+
+  template<class T>
+      typename unique_if<T>::unknown_bound
+      make_unique(size_t n) {
+          typedef typename std::remove_extent<T>::type U;
+          return std::unique_ptr<T>(new U[n]());
+      }
+
+  template<class T, class... Args>
+      typename unique_if<T>::known_bound
+      make_unique(Args&&...) = delete;
+
+  template<class T>
+	typename unique_if<T>::unknown_bound
+	make_unique_uninitialized(size_t n) {
+		typedef typename std::remove_extent<T>::type U;
+		return std::unique_ptr<T>(new U[n]);
+	}
+
+}
+#endif /* MAKE_UNIQUE_H_ */
