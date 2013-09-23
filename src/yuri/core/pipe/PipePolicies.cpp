e156ab983fb5f0f6e6a43d63d70eb5370f98f20d
Zdenek Travnicek <travnicek@iim.cz>
2013-09-24 01:58:37 +0200

    Added pipe policies for special pipes.

---
+++ src/yuri/core/pipe/PipePolicies.cpp
@@ -0,0 +1,70 @@
+/*
+ * PipePolicies.cpp
+ *
+ *  Created on: 25.11.2012
+ *      Author: neneko
+ */
+
+#include "PipePolicies.h"
+#include <cassert>
+
+namespace yuri {
+namespace core {
+namespace pipe {
+
+template<>
+bool SingleFramePolicy<false>::impl_push_frame(const pFrame &frame)
+{
+	frame_ = frame;
+	return true;
+}
+template<>
+bool SingleFramePolicy<true>::impl_push_frame(const pFrame &frame)
+{
+	if (frame_) return false;
+	frame_ = frame;
+	return true;
+}
+
+template<>
+bool SizeLimitedPolicy<false>::impl_push_frame(const pFrame &frame)
+{
+	frames_.push_back(frame);
+	actual_size_+=frame->get_size();
+	while (actual_size_>max_size_) {
+		assert(frames_.size());
+		pFrame f = frames_.front();
+		frames_.pop_front();
+		actual_size_-=f->get_size();
+	}
+	return true;
+}
+template<>
+bool SizeLimitedPolicy<true>::impl_push_frame(const pFrame &frame)
+{
+	if ((actual_size_ + frame->get_size()) > max_size_) return false;
+	frames_.push_back(frame);
+	actual_size_+=frame->get_size();
+	return true;
+}
+
+template<>
+bool CountLimitedPolicy<false>::impl_push_frame(const pFrame &frame)
+{
+	frames_.push_back(frame);
+	while (frames_.size()>max_count_) frames_.pop_front();
+	return true;
+}
+
+template<>
+bool CountLimitedPolicy<true>::impl_push_frame(const pFrame &frame)
+{
+	if (frames_.size()>=max_count_) return false;
+	frames_.push_back(frame);
+	return true;
+}
+
+
+}
+} /* namespace core */
+} /* namespace yuri */
---
+++ src/yuri/core/pipe/PipePolicies.h
@@ -0,0 +1,150 @@
+/*
+ * PipePolicies.h
+ *
+ *  Created on: 25.11.2012
+ *      Author: neneko
+ */
+
+#ifndef PIPEPOLICIES_H_
+#define PIPEPOLICIES_H_
+#include "yuri/core/utils/new_types.h"
+#include "yuri/core/frame/Frame.h"
+#include "yuri/core/parameter/Parameters.h"
+#include <deque>
+
+namespace yuri {
+namespace core {
+
+namespace pipe {
+
+/*!
+ * \brief Policy for pipes without any storage limit
+ */
+template<bool blocking>
+class UnlimitedPolicy {
+public:
+	static Parameters configure() {return Parameters{};}
+protected:
+	UnlimitedPolicy(const Parameters&) {}
+	~UnlimitedPolicy() noexcept {}
+	bool impl_push_frame(const pFrame &frame)
+	{
+		frames_.push_back(frame);
+		return true;
+	}
+	virtual pFrame impl_pop_frame()
+	{
+		pFrame frame;
+		if (frames_.empty()) return frame;
+		frame = frames_.front();
+		frames_.pop_front();
+		return frame;
+	}
+	size_t impl_get_size() const {
+		return frames_.size();
+	}
+private:
+	std::deque<pFrame> frames_;
+};
+
+/*!
+ * Policy for pipes able to hold only a single frame
+ */
+
+template<bool blocking>
+class SingleFramePolicy {
+public:
+	static Parameters configure() {return Parameters{};}
+protected:
+	SingleFramePolicy(const Parameters&) {}
+	~SingleFramePolicy() noexcept {}
+	bool impl_push_frame(const pFrame &frame);
+	pFrame impl_pop_frame()
+	{
+		pFrame frame = frame_;
+		frame_.reset();
+		return frame;
+	}
+	size_t impl_get_size() const {
+		return frame_?1:0;
+	}
+private:
+	pFrame frame_;
+};
+
+/*!
+ * Policy for pipes able to store limited number of frames
+ */
+template<bool blocking>
+class SizeLimitedPolicy {
+public:
+	static Parameters configure() {return Parameters{};}
+protected:
+	SizeLimitedPolicy(const Parameters&):actual_size_(0),max_size_(0)
+	{
+		/*! @TODO: Process parameters */
+	}
+	virtual ~SizeLimitedPolicy() noexcept {}
+	void set_size_limit(yuri::size_t max_size)
+	{
+		max_size_ = max_size;
+	}
+	bool impl_push_frame(const pFrame &frame);
+	pFrame impl_pop_frame()
+	{
+		pFrame frame;
+		if (frames_.empty()) return frame;
+		frame = frames_.front();
+		frames_.pop_front();
+		actual_size_-= frame->get_size();
+		return frame;
+	}
+	size_t impl_get_size() const {
+		return frames_.size();
+	}
+private:
+	std::deque<pFrame> frames_;
+	yuri::size_t actual_size_;
+	yuri::size_t max_size_;
+};
+
+/*!
+ * @brief Policy for pipes able to store only limited amount of data
+ */
+template<bool blocking>
+class CountLimitedPolicy {
+public:
+	static Parameters configure() {return Parameters{};}
+protected:
+	CountLimitedPolicy(const Parameters&):max_count_(0)
+	{
+		/*! @TODO: Process parameters */
+	}
+	virtual ~CountLimitedPolicy() noexcept {}
+	void set_count_limit(yuri::size_t max_count)
+	{
+		max_count_ = max_count;
+	}
+	bool impl_push_frame(const pFrame &frame);
+	pFrame impl_pop_frame()
+	{
+		pFrame frame;
+		if (frames_.empty()) return frame;
+		frame = frames_.front();
+		frames_.pop_front();
+		return frame;
+	}
+	size_t impl_get_size() const {
+		return frames_.size();
+	}
+private:
+	std::deque<pFrame> frames_;
+	yuri::size_t max_count_;
+};
+
+
+}
+
+} /* namespace core */
+} /* namespace yuri */
+#endif /* PIPEPOLICIES_H_ */
