8fb28a2db84c0d6d6f3e0957216a3d68d66df237
Zdenek Travnicek <travnicek@iim.cz>
2013-09-24 01:53:15 +0200

    Added new Parameters implementation.

---
+++ src/yuri/core/parameter/Parameters.cpp
@@ -0,0 +1,88 @@
+/*
+ * Parameters.cpp
+ *
+ *  Created on: 11.9.2013
+ *      Author: neneko
+ */
+
+#include "Parameters.h"
+
+namespace yuri {
+namespace core {
+
+Parameters&	Parameters::merge(const Parameters& rhs)
+{
+//	params_.insert(rhs.begin(), rhs.end());
+	for(auto& p: rhs) {
+			auto it = params_.find(p.first);
+			if (it!=params_.end()) {
+				it->second = p.second;
+			} else {
+	#if EMPLACE_UNSUPPORTED
+				params_.insert(std::make_pair(p.first,p.second));
+	#else
+				params_.emplace(p.first,p.second);
+	#endif
+			}
+		}
+	return *this;
+}
+
+Parameters&	Parameters::merge(Parameters&& rhs)
+{
+	for(auto& p: rhs) {
+		auto it = params_.find(p.first);
+			if (it!=params_.end()) {
+				it->second = p.second;
+			} else {
+#if EMPLACE_UNSUPPORTED
+				params_.insert(std::make_pair(p.first,std::move(p.second)));
+#else
+			params_.emplace(p.first,std::move(p.second));
+#endif
+		}
+	}
+	return *this;
+}
+
+void Parameters::set_description(const std::string& description)
+{
+	description_ = description;
+}
+const std::string& Parameters::get_description() const
+{
+	return description_;
+}
+
+Parameter& Parameters::set_parameter(const Parameter& param)
+{
+#if EMPLACE_UNSUPPORTED
+	auto it = params_.insert(std::make_pair(param.get_name(),param));
+#else
+	auto it = params_.emplace(param.get_name(),param);
+#endif
+	return it.first->second;
+}
+Parameter& Parameters::set_parameter(Parameter&& param)
+{
+#if EMPLACE_UNSUPPORTED
+	auto it = params_.insert(std::make_pair(param.get_name(),std::move(param)));
+#else
+	auto it = params_.emplace(param.get_name(),std::move(param));
+#endif
+	return it.first->second;
+}
+
+Parameter& Parameters::operator[](const std::string& name)
+{
+	return set_parameter(Parameter(name));
+}
+const Parameter& Parameters::operator[](const std::string& name) const
+{
+	const auto it = params_.find(name);
+	if (it==params_.end()) throw std::runtime_error("Not such a value");
+	return it->second;
+}
+
+}
+}
---
+++ src/yuri/core/parameter/Parameters.h
@@ -0,0 +1,60 @@
+/*
+ * Parameters.h
+ *
+ *  Created on: 8.9.2013
+ *      Author: neneko
+ */
+
+#ifndef PARAMETERS_H_
+#define PARAMETERS_H_
+#include "Parameter.h"
+
+#include <map>
+
+namespace yuri {
+namespace core {
+
+class Parameters {
+public:
+	using map_type 				= std::map<std::string, Parameter>;
+	using iterator 				= map_type::iterator;
+	using const_iterator 		= map_type::const_iterator;
+
+								Parameters(const std::string& description = std::string()):description_(description) {}
+								Parameters(Parameters&& rhs):params_(std::move(rhs.params_)),description_(std::move(rhs.description_)){}
+	Parameters&					operator=(Parameters&& rhs) {params_=std::move(rhs.params_);description_ = std::move(rhs.description_);return *this;}
+								~Parameters() noexcept {}
+	Parameters&					merge(const Parameters& rhs);
+	Parameters&					merge(Parameters&& rhs);
+	void						set_description(const std::string& description);
+	const std::string&			get_description() const;
+	Parameter&					set_parameter(const Parameter& param);
+	Parameter&					set_parameter(Parameter&& param);
+
+	template<typename T>
+	void
+								set_parameter(const std::string& name, const T& value)
+	{
+		set_parameter(Parameter(name, value));
+	}
+
+	Parameter&					operator[](const std::string& name);
+	const Parameter&			operator[](const std::string& name) const;
+
+	iterator					begin() { return params_.begin(); }
+	const_iterator				begin() const { return params_.begin(); }
+	const_iterator				cbegin() { return params_.cbegin(); }
+	iterator					end() { return params_.end(); }
+	const_iterator				end() const { return params_.end(); }
+	const_iterator				cend() { return params_.cend(); }
+private:
+	map_type					params_;
+	std::string					description_;
+};
+
+
+}
+}
+
+
+#endif /* PARAMETERS_H_ */
