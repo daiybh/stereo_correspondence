d6f79c8962a7ff032cb3a8d30da99659ce7f5348
Zdenek Travnicek <travnicek@iim.cz>
2013-09-24 01:30:18 +0200

    Added Raw video frame implementation.

---
+++ src/yuri/core/frame/RawVideoFrame.cpp
@@ -0,0 +1,101 @@
+/*
+ * RawVideoFrame.cpp
+ *
+ *  Created on: 8.9.2013
+ *      Author: neneko
+ */
+
+#include "RawVideoFrame.h"
+#include "raw_frame_types.h"
+#include "raw_frame_params.h"
+#include "yuri/core/thread/FixedMemoryAllocator.h"
+namespace yuri {
+namespace core  {
+
+
+pRawVideoFrame RawVideoFrame::create_empty(format_t format, resolution_t resolution, bool fixed, interlace_t interlace, field_order_t field_order)
+{
+	pRawVideoFrame frame;
+	try {
+		const auto& info = raw_format::get_format_info(format);
+//		size_t planes = info.planes.size();
+		// Creating with 0 planes and them emplacing planes into it.
+		frame = make_shared<RawVideoFrame>(format, resolution, 0);
+		for (const auto& p: info.planes) {
+			const size_t line_size_nom = resolution.width * p.bit_depth.first;
+			const size_t line_size_den = p.bit_depth.second * p.sub_x * 8;
+			const size_t line_size_unaligned = line_size_nom / line_size_den + line_size_nom % line_size_den;
+			const size_t line_size = p.alignment_requirement?line_size_unaligned+line_size_unaligned%p.alignment_requirement:line_size_unaligned;
+			const size_t frame_size = line_size * resolution.height / p.sub_y;
+			if (!fixed) {
+				frame->emplace_back(frame_size, resolution, line_size);
+			} else {
+				auto mem = FixedMemoryAllocator::get_block(frame_size);
+				Plane::vector_type data{mem.first, frame_size, mem.second};
+				frame->emplace_back(std::move(data), resolution, line_size);
+			}
+
+		}
+		frame->set_interlacing(interlace);
+		frame->set_field_order(field_order);
+
+	}
+	catch (std::runtime_error&) {}
+	return frame;
+
+}
+
+RawVideoFrame::RawVideoFrame(format_t format, resolution_t resolution, size_t plane_count)
+:VideoFrame(format, resolution)
+{
+	set_planes_count(plane_count);
+}
+RawVideoFrame::~RawVideoFrame() noexcept
+{
+
+}
+
+void RawVideoFrame::set_planes_count(index_t count)
+{
+	Plane p(0, {0, 0}, 0);
+	planes_.resize(count, p);
+}
+
+void RawVideoFrame::push_back(const Plane& plane)
+{
+	planes_.push_back(plane);
+}
+void RawVideoFrame::push_back(Plane&& plane)
+{
+	planes_.push_back(plane);
+}
+
+pFrame RawVideoFrame::do_get_copy() const {
+	pRawVideoFrame frame = make_shared<RawVideoFrame>(get_format(), get_resolution());
+	RawVideoFrame& rvframe = *frame;
+	copy_parameters(rvframe);
+	std::copy(begin(),end(),rvframe.begin());
+	return frame;
+}
+
+size_t RawVideoFrame::do_get_size() const noexcept
+{
+	return std::accumulate(planes_.begin(), planes_.end(), 0,
+			[](const size_t& a, const Plane& plane){return a + plane.get_size();});
+}
+
+void RawVideoFrame::copy_parameters(Frame& other) const {
+	try {
+		RawVideoFrame& frame = dynamic_cast<RawVideoFrame&>(other);
+		frame.set_planes_count(get_planes_count());
+	}
+	catch (std::bad_cast&) {
+		throw std::runtime_error("Tried to set VideoFrame params to a type not related to VideoFrame");
+	}
+	VideoFrame::copy_parameters(other);
+}
+}
+}
+
+
+
---
+++ src/yuri/core/frame/RawVideoFrame.h
@@ -0,0 +1,89 @@
+/*
+ * RawVideoFrame.h
+ *
+ *  Created on: 30.7.2013
+ *      Author: neneko
+ */
+
+#ifndef RAWVIDEOFRAME_H_
+#define RAWVIDEOFRAME_H_
+#include "VideoFrame.h"
+#include "Plane.h"
+#include <vector>
+namespace yuri {
+namespace core {
+class RawVideoFrame;
+typedef shared_ptr<RawVideoFrame> pRawVideoFrame;
+#define PLANE_DATA(pframe, idx) (*pframe)[idx]
+
+class RawVideoFrame: public VideoFrame
+{
+public:
+	typedef	Plane				value_type;
+	typedef std::vector<value_type>
+								vector_type;
+	typedef typename vector_type::iterator
+								iterator;
+	typedef typename vector_type::const_iterator
+								const_iterator;
+	typedef typename vector_type::reference
+								reference;
+	typedef typename vector_type::const_reference
+								const_reference;
+
+	static pRawVideoFrame create_empty(format_t, resolution_t, bool fixed = false, interlace_t interlace = interlace_t::progressive, field_order_t filed_order = field_order_t::none);
+
+	RawVideoFrame(format_t format, resolution_t resolution, size_t plane_count = 1);
+	virtual ~RawVideoFrame() noexcept;
+
+//	Plane&	operator[](index_t index);
+
+	size_t			get_planes_count() const { return planes_.size(); }
+	void			set_planes_count(index_t count);
+
+
+	iterator					begin() {return planes_.begin();}
+	iterator					end() {return planes_.end();}
+	const_iterator				begin() const {return planes_.begin();}
+	const_iterator				end() const {return planes_.end();}
+	const_iterator				cbegin() const {return planes_.cbegin();}
+	const_iterator				cend() const {return planes_.cend();}
+	iterator					data() { return begin(); }
+	const_iterator				data() const { return begin(); }
+	reference					operator[](index_t index) { return planes_[index]; }
+	const_reference				operator[](index_t index) const { return planes_[index]; }
+	size_t						size() const { return planes_.size(); }
+	void						push_back(const Plane& plane);
+	void						push_back(Plane&& plane);
+	template<class... Args>
+	void						emplace_back(Args&&... args) { planes_.emplace_back(std::forward<Args>(args)...); }
+
+private:
+	/*!
+	 * Implementation of copy, should be implemented in node classes only.
+	 * @return Copy of current frame
+	 */
+	virtual pFrame	do_get_copy() const;
+	/*!
+	 * Implementation od get_size() method
+	 * @return Size of current frame
+	 */
+	virtual size_t	do_get_size() const noexcept;
+protected:
+	/*!
+	 * Copies parameters from the frame to other frame.
+	 * The intention is to be used in get_copy() method.
+	 * @param frame to copy parameters to
+	 */
+	virtual void	copy_parameters(Frame&) const;
+private:
+	vector_type		planes_;
+
+};
+
+}
+}
+
+
+
+#endif /* RAWVIDEOFRAME_H_ */
