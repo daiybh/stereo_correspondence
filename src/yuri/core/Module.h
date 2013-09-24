bcb57972d5172cc2c6fb81e1fd4a161ef1ec137a
Zdenek Travnicek <travnicek@iim.cz>
2013-09-24 02:28:23 +0200

    Added new Module.h

---
+++ src/yuri/core/Module.h
@@ -0,0 +1,18 @@
+/*
+ * Module.h
+ *
+ *  Created on: 22.2.2013
+ *      Author: neneko
+ */
+
+#ifndef MODULE_H_
+#define MODULE_H_
+
+#include "yuri/core/utils/platform.h"
+#include "yuri/exception/InitializationFailed.h"
+#include "yuri/core/parameter/Parameters.h"
+#include "yuri/core/pipe/Pipe.h"
+#include "yuri/core/frame/Frame.h"
+#include "yuri/core/thread/IOThreadGenerator.h"
+
+#endif /* MODULE_H_ */
