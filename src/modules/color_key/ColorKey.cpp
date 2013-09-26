701e194d6edf5e4161c985171f70d297fd78375d
Zdenek Travnicek <travnicek@iim.cz>
2013-09-26 02:32:37 +0200

    Ported color_key to version 2.8

--- src/modules/color_key/ColorKey.cpp
+++ src/modules/color_key/ColorKey.cpp
@@ -9,50 +9,53 @@
 
 #include "ColorKey.h"
 #include "yuri/core/Module.h"
-#include <boost/assign.hpp>
+#include "yuri/core/frame/raw_frame_params.h"
+#include "yuri/core/frame/raw_frame_types.h"
 namespace yuri {
 namespace color_key {
 
-REGISTER("color_key",ColorKey)
+IOTHREAD_GENERATOR(ColorKey)
 
-IO_THREAD_GENERATOR(ColorKey)
+MODULE_REGISTRATION_BEGIN("color_key")
+		REGISTER_IOTHREAD("color_key",ColorKey)
+MODULE_REGISTRATION_END()
 
-core::pParameters ColorKey::configure()
+core::Parameters ColorKey::configure()
 {
-	core::pParameters p = core::BasicIOThread::configure();
-	p->set_description("ColorKey");
-	p->set_max_pipes(1,1);
-	(*p)["r"]["R value of the key color"]=0;
-	(*p)["g"]["G value of the key color"]=0;
-	(*p)["b"]["B value of the key color"]=0;
-	(*p)["delta"]["Threshold for determining same colors"]=90;
-	(*p)["delta2"]["Threshold for determining similar colors"]=30;
-	(*p)["diff"]["Method for computing differences (linear, quadratic)"]="linear";
+	core::Parameters p = core::IOFilter::configure();
+	p.set_description("ColorKey");
+//	p->set_max_pipes(1,1);
+	p["r"]["R value of the key color"]=0;
+	p["g"]["G value of the key color"]=0;
+	p["b"]["B value of the key color"]=0;
+	p["delta"]["Threshold for determining same colors"]=90;
+	p["delta2"]["Threshold for determining similar colors"]=30;
+	p["diff"]["Method for computing differences (linear, quadratic)"]="linear";
 	return p;
 }
 
 
-ColorKey::ColorKey(log::Log &log_, core::pwThreadBase parent, core::Parameters &parameters):
-core::BasicIOThread(log_,parent,1,1,std::string("color_key")),r_(140),g_(200),b_(75),
+ColorKey::ColorKey(log::Log &log_, core::pwThreadBase parent, const core::Parameters &parameters):
+core::IOFilter(log_,parent,std::string("color_key")),r_(140),g_(200),b_(75),
 delta_(100),delta2_(30),diff_type_(linear)
 {
-	IO_THREAD_INIT("color_key")
+	IOTHREAD_INIT(parameters)
 }
 
-ColorKey::~ColorKey()
+ColorKey::~ColorKey() noexcept
 {
 }
 namespace {
 
-std::map<std::string, diff_types_> diff_type_strings = boost::assign::map_list_of<std::string, diff_types_>
+std::map<std::string, diff_types_> diff_type_strings = map_list_of<std::string, diff_types_>
 ("linear", 		linear)
 ("quadratic",	quadratic);
 
-inline ubyte_t diff(ubyte_t a, ubyte_t b)
+inline uint8_t diff(uint8_t a, uint8_t b)
 {
 	return a>b?a-b:b-a;
 }
-inline bool same_color(plane_t::const_iterator data, ubyte_t r, ubyte_t g, ubyte_t b, ushort_t delta)
+inline bool same_color(core::RawVideoFrame::value_type::const_iterator data, uint8_t r, uint8_t g, uint8_t b, ssize_t delta)
 {
 	if (diff(*data++,r)>delta) return false;
 	if (diff(*data++,g)>delta) return false;
@@ -60,12 +63,12 @@ inline bool same_color(plane_t::const_iterator data, ubyte_t r, ubyte_t g, ubyte
 	return true;
 }
 struct diff_linear {
-	static ushort_t combine(ubyte_t a, ubyte_t b, ubyte_t c) {
+	static ssize_t combine(uint8_t a, uint8_t b, uint8_t c) {
 		return a + b + c;
 	}
 };
 struct diff_quad {
-	static ushort_t combine(ubyte_t a, ubyte_t b, ubyte_t c) {
+	static ssize_t combine(uint8_t a, uint8_t b, uint8_t c) {
 		return a * a + b * b + c * c;
 	}
 };
@@ -73,8 +76,8 @@ template<format_t, class diff_method>
 struct simple_kernel {};
 
 template<class diff_method>
-struct simple_kernel<YURI_FMT_RGB, diff_method> {
-static void eval(plane_t::const_iterator& src_pix, plane_t::iterator& dest_pix, ushort_t total, ushort_t delta, ushort_t delta2)
+struct simple_kernel<core::raw_format::rgb24, diff_method> {
+static void eval(core::RawVideoFrame::value_type::const_iterator& src_pix, core::RawVideoFrame::value_type::iterator& dest_pix, ssize_t total, ssize_t delta, ssize_t delta2)
 {
 	if (total < delta) {
 		*dest_pix++=255;
@@ -87,7 +90,7 @@ static void eval(plane_t::const_iterator& src_pix, plane_t::iterator& dest_pix, 
 		*dest_pix++=*src_pix++;
 		*dest_pix++=*src_pix++;
 		*dest_pix++=*src_pix++;
-		*dest_pix++=static_cast<ubyte_t>(255*a);
+		*dest_pix++=static_cast<uint8_t>(255*a);
 	} else {
 		*dest_pix++=*src_pix++;
 		*dest_pix++=*src_pix++;
@@ -95,14 +98,14 @@ static void eval(plane_t::const_iterator& src_pix, plane_t::iterator& dest_pix, 
 		*dest_pix++=255;
 	}
 }
-static ushort_t difference(plane_t::const_iterator data, ubyte_t r, ubyte_t g, ubyte_t b)
+static ssize_t difference(core::RawVideoFrame::value_type::const_iterator data, uint8_t r, uint8_t g, uint8_t b)
 {
 	return diff_method::combine(diff(*(data+0),r), diff(*(data+1),g), diff(*(data+2),b));
 }
 };
 template<class diff_method>
-struct simple_kernel<YURI_FMT_RGBA, diff_method> {
-static void eval(plane_t::const_iterator& src_pix, plane_t::iterator& dest_pix, ushort_t total, ushort_t delta, ushort_t delta2)
+struct simple_kernel<core::raw_format::rgba32, diff_method> {
+static void eval(core::RawVideoFrame::value_type::const_iterator& src_pix, core::RawVideoFrame::value_type::iterator& dest_pix, ssize_t total, ssize_t delta, ssize_t delta2)
 {
 	if (total < delta) {
 		*dest_pix++=255;
@@ -115,7 +118,7 @@ static void eval(plane_t::const_iterator& src_pix, plane_t::iterator& dest_pix, 
 		*dest_pix++=*src_pix++;
 		*dest_pix++=*src_pix++;
 		*dest_pix++=*src_pix++;
-		*dest_pix++=static_cast<ubyte_t>(*src_pix++*a);
+		*dest_pix++=static_cast<uint8_t>(*src_pix++*a);
 	} else {
 		*dest_pix++=*src_pix++;
 		*dest_pix++=*src_pix++;
@@ -123,25 +126,25 @@ static void eval(plane_t::const_iterator& src_pix, plane_t::iterator& dest_pix, 
 		*dest_pix++=*src_pix++;
 	}
 }
-static ushort_t difference(plane_t::const_iterator data, ubyte_t r, ubyte_t g, ubyte_t b)
+static ssize_t difference(core::RawVideoFrame::value_type::const_iterator data, uint8_t r, uint8_t g, uint8_t b)
 {
 	return diff_method::combine(diff(*(data+0),r), diff(*(data+1),g), diff(*(data+2),b));
 }
 };
 template<class diff_method>
-struct simple_kernel<YURI_FMT_BGR, diff_method>:
-public simple_kernel<YURI_FMT_RGB, diff_method>{
+struct simple_kernel<core::raw_format::bgr24, diff_method>:
+public simple_kernel<core::raw_format::rgb24, diff_method>{
 
-static ushort_t difference(plane_t::const_iterator data, ubyte_t r, ubyte_t g, ubyte_t b)
+static ssize_t difference(core::RawVideoFrame::value_type::const_iterator data, uint8_t r, uint8_t g, uint8_t b)
 {
 	return diff_method::combine(diff(*(data+0),b), diff(*(data+1),g), diff(*(data+2),r));
 }
 };
 template<class diff_method>
-struct simple_kernel<YURI_FMT_BGRA, diff_method>:
-public simple_kernel<YURI_FMT_RGBA, diff_method>{
+struct simple_kernel<core::raw_format::abgr32, diff_method>:
+public simple_kernel<core::raw_format::rgba32, diff_method>{
 
-static ushort_t difference(plane_t::const_iterator data, ubyte_t r, ubyte_t g, ubyte_t b)
+static ssize_t difference(core::RawVideoFrame::value_type::const_iterator data, uint8_t r, uint8_t g, uint8_t b)
 {
 	return diff_method::combine(diff(*(data+0),b), diff(*(data+1),g), diff(*(data+2),r));
 }
@@ -149,20 +152,21 @@ static ushort_t difference(plane_t::const_iterator data, ubyte_t r, ubyte_t g, u
 
 }
 template<class kernel>
-core::pBasicFrame ColorKey::find_key(const core::pBasicFrame& frame)
+core::pRawVideoFrame ColorKey::find_key(const core::pRawVideoFrame& frame)
 {
-	const size_t 			width 		= frame->get_width();
-	const size_t 			height 		= frame->get_height();
+	const resolution_t		res 		= frame->get_resolution();
+	const size_t 			width 		= res.width;
+	const size_t 			height 		= res.height;
 	const size_t 			linesize_in = width * 3;
 	const size_t 			linesize_out= width * 4;
-	core::pBasicFrame 		outframe 	= allocate_empty_frame(YURI_FMT_RGBA, width, height);
-	const plane_t::const_iterator src 		= PLANE_DATA(frame,0).begin();
-	const plane_t::iterator 		dest 		= PLANE_DATA(outframe,0).begin();
+	core::pRawVideoFrame	outframe 	= core::RawVideoFrame::create_empty(core::raw_format::rgba32, res);
+	const core::RawVideoFrame::value_type::const_iterator src 		= PLANE_DATA(frame,0).begin();
+	const core::RawVideoFrame::value_type::iterator 	dest 		= PLANE_DATA(outframe,0).begin();
 	for (size_t line = 0; line < height; ++line) {
-		plane_t::const_iterator src_pix 	= src+line*linesize_in;
-		plane_t::iterator 		dest_pix	= dest+line*linesize_out;
+		core::RawVideoFrame::value_type::const_iterator src_pix 	= src+line*linesize_in;
+		core::RawVideoFrame::value_type::iterator 		dest_pix	= dest+line*linesize_out;
 		for (size_t pixel = 0; pixel < width; ++pixel) {
-			const ushort_t total = kernel::difference(src_pix, r_, g_, b_);
+			const ssize_t total = kernel::difference(src_pix, r_, g_, b_);
 			kernel::eval(src_pix, dest_pix, total, delta_, delta2_);
 		}
 	}
@@ -170,7 +174,7 @@ core::pBasicFrame ColorKey::find_key(const core::pBasicFrame& frame)
 }
 
 template<format_t format>
-core::pBasicFrame ColorKey::dispatch_find_key(const core::pBasicFrame& frame)
+core::pRawVideoFrame ColorKey::dispatch_find_key(const core::pRawVideoFrame& frame)
 {
 	switch (diff_type_) {
 		case linear:
@@ -178,55 +182,56 @@ core::pBasicFrame ColorKey::dispatch_find_key(const core::pBasicFrame& frame)
 		case quadratic:
 			return find_key<simple_kernel<format, diff_quad> >(frame);
 	}
-	return core::pBasicFrame();
+	return core::pRawVideoFrame();
 }
-bool ColorKey::step()
+//bool ColorKey::step()
+core::pFrame ColorKey::do_simple_single_step(const core::pFrame& gframe)
 {
-	core::pBasicFrame frame = in[0]->pop_frame();
+	core::pRawVideoFrame frame = dynamic_pointer_cast<core::RawVideoFrame>(gframe);
 	if (frame) {
 		const format_t fmt = frame->get_format();
-		core::pBasicFrame outframe;
+		core::pRawVideoFrame outframe;
 		switch (fmt) {
-			case YURI_FMT_RGB:
-				outframe = dispatch_find_key<YURI_FMT_RGB>(frame);
+			case core::raw_format::rgb24:
+				outframe = dispatch_find_key<core::raw_format::rgb24>(frame);
 				break;
-			case YURI_FMT_BGR:
-				outframe = dispatch_find_key<YURI_FMT_BGR>(frame);
+			case core::raw_format::bgr24:
+				outframe = dispatch_find_key<core::raw_format::bgr24>(frame);
 				break;
-			case YURI_FMT_RGBA:
-				outframe = dispatch_find_key<YURI_FMT_RGBA>(frame);
+			case core::raw_format::rgba32:
+				outframe = dispatch_find_key<core::raw_format::rgba32>(frame);
 				break;
-			case YURI_FMT_BGRA:
-				outframe = dispatch_find_key<YURI_FMT_BGRA>(frame);
+			case core::raw_format::abgr32:
+				outframe = dispatch_find_key<core::raw_format::abgr32>(frame);
 				break;
 			default:
 				log[log::warning] << "Unsupported frame format";
-				return true;
+				return {};
 		}
 
-		if (outframe) push_raw_video_frame(0, outframe);
+		if (outframe) push_frame(0, outframe);
 	}
-	return true;
+	return {};
 }
 bool ColorKey::set_param(const core::Parameter& param)
 {
 //	using boost::iequals;
-	if (iequals(param.name,"r")) {
-		r_ = param.get<ushort_t>();
-	} else if (iequals(param.name,"g")) {
-		g_ = param.get<ushort_t>();
-	} else if (iequals(param.name,"b")) {
-		b_ = param.get<ushort_t>();
-	} else if (iequals(param.name,"delta")) {
-		delta_ = param.get<ushort_t>();
-	} else if (iequals(param.name,"delta2")) {
-		delta2_ = param.get<ushort_t>();
-	} else if (iequals(param.name,"diff")) {
+	if (iequals(param.get_name(),"r")) {
+		r_ = param.get<ssize_t>();
+	} else if (iequals(param.get_name(),"g")) {
+		g_ = param.get<ssize_t>();
+	} else if (iequals(param.get_name(),"b")) {
+		b_ = param.get<ssize_t>();
+	} else if (iequals(param.get_name(),"delta")) {
+		delta_ = param.get<ssize_t>();
+	} else if (iequals(param.get_name(),"delta2")) {
+		delta2_ = param.get<ssize_t>();
+	} else if (iequals(param.get_name(),"diff")) {
 		const std::string dt = param.get<std::string>();
 		if (diff_type_strings.count(dt)) {
 			diff_type_ = diff_type_strings[dt];
 		} else diff_type_ = linear;
-	} else return core::BasicIOThread::set_param(param);
+	} else return core::IOThread::set_param(param);
 	return true;
 }
 
--- src/modules/color_key/ColorKey.h
+++ src/modules/color_key/ColorKey.h
@@ -10,7 +10,9 @@
 #ifndef COLORKEY_H_
 #define COLORKEY_H_
 
-#include "yuri/core/BasicIOThread.h"
+#include "yuri/core/thread/IOFilter.h"
+#include "yuri/core/frame/RawVideoFrame.h"
+
 
 namespace yuri {
 namespace color_key {
@@ -21,22 +23,24 @@ enum diff_types_ {
 };
 
 
-class ColorKey: public core::BasicIOThread
+class ColorKey: public core::IOFilter
 {
 public:
-	IO_THREAD_GENERATOR_DECLARATION
-	static core::pParameters configure();
-	virtual ~ColorKey();
+	IOTHREAD_GENERATOR_DECLARATION
+	static core::Parameters configure();
+	ColorKey(log::Log &log_, core::pwThreadBase parent, const core::Parameters &parameters);
+	virtual ~ColorKey() noexcept;
 private:
-	ColorKey(log::Log &log_, core::pwThreadBase parent, core::Parameters &parameters);
-	virtual bool step();
+
+//	virtual bool step();
+	virtual core::pFrame do_simple_single_step(const core::pFrame& frame) override;
 	virtual bool set_param(const core::Parameter& param);
 	template<class kernel>
-	core::pBasicFrame find_key(const core::pBasicFrame& frame);
+	core::pRawVideoFrame find_key(const core::pRawVideoFrame& frame);
 	template<format_t format>
-	core::pBasicFrame dispatch_find_key(const core::pBasicFrame& frame);
-	ubyte_t r_, g_, b_;
-	ushort_t delta_, delta2_;
+	core::pRawVideoFrame dispatch_find_key(const core::pRawVideoFrame& frame);
+	uint8_t r_, g_, b_;
+	ssize_t delta_, delta2_;
 	diff_types_ diff_type_;
 };
 
