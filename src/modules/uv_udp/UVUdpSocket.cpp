7b8b06efbf81c601558c83381838deafaf8d00d1
Zdenek Travnicek <travnicek@iim.cz>
2013-10-16 15:50:43 +0200

    Added validity checking to uv_udp.

--- src/modules/uv_udp/UVUdpSocket.cpp
+++ src/modules/uv_udp/UVUdpSocket.cpp
@@ -35,23 +35,26 @@ UVUdpSocket::~UVUdpSocket() noexcept
 size_t UVUdpSocket::do_send_datagram(const uint8_t* data, size_t size)
 {
 	// !TODO Should be fixed upstream for const correctness) and const_cast removed!
+	if (socket_.get()==nullptr) throw core::socket::socket_not_connected();
 	return udp_send(socket_.get(),
 			const_cast<char*>(reinterpret_cast<const char*>(data)),
 			static_cast<int>(size));
 }
 
 size_t UVUdpSocket::do_receive_datagram(uint8_t* data, size_t size) {
+	if (socket_.get()==nullptr) throw core::socket::socket_not_connected();
 	return udp_recv(socket_.get(),
 			reinterpret_cast<char*>(data),
 			static_cast<int>(size));
 }
 
 bool UVUdpSocket::do_bind(const std::string& url, core::socket::port_t port) {
-	socket_.reset(udp_init(url.c_str(),port,0,255,false));
+	socket_.reset(udp_init(/*url.c_str()*/nullptr,port,0,255,false));
 	return socket_.get();
 }
 
 bool UVUdpSocket::do_data_available() {
+	if (socket_.get()==nullptr) throw core::socket::socket_not_connected();
 	char c;
 	return udp_peek(socket_.get(),&c, 1)!=0;
 }
