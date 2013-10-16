951d720a457f0e181bafd5e819ba55c22e372d1e
Zdenek Travnicek <travnicek@iim.cz>
2013-10-16 15:51:34 +0200

    Ported osc_receiver.

--- src/modules/osc_receiver/OSCReceiver.cpp
+++ src/modules/osc_receiver/OSCReceiver.cpp
@@ -9,33 +9,37 @@
 
 #include "OSCReceiver.h"
 #include "yuri/core/Module.h"
+#include "yuri/core/socket/DatagramSocketGenerator.h"
 
 namespace yuri {
 namespace osc_receiver {
 
-REGISTER("osc_receiver",OSCReceiver)
+IOTHREAD_GENERATOR(OSCReceiver)
 
-IO_THREAD_GENERATOR(OSCReceiver)
+MODULE_REGISTRATION_BEGIN("osc_receiver")
+		REGISTER_IOTHREAD("osc_receiver",OSCReceiver)
+MODULE_REGISTRATION_END()
 
-core::pParameters OSCReceiver::configure()
+core::Parameters OSCReceiver::configure()
 {
-	core::pParameters p = core::BasicIOThread::configure();
-	p->set_description("OSCReceiver");
-	p->set_max_pipes(0,0);
+	core::Parameters p = core::IOThread::configure();
+	p.set_description("OSCReceiver");
+	p["socket_type"]="uv_udp";
+	//p->set_max_pipes(0,0);
 	return p;
 }
 
 
-OSCReceiver::OSCReceiver(log::Log &log_, core::pwThreadBase parent, core::Parameters &parameters):
-core::BasicIOThread(log_,parent,0,0,std::string("osc_receiver")),
-event::BasicEventProducer(log),port_(2000)
+OSCReceiver::OSCReceiver(const log::Log &log_, core::pwThreadBase parent, const core::Parameters &parameters):
+core::IOThread(log_,parent,0,0,std::string("osc_receiver")),
+event::BasicEventProducer(log),port_(2000),socket_type_("uv_udp")
 {
-	IO_THREAD_INIT("osc_receiver")
+	IOTHREAD_INIT(parameters)
 
 
 }
 
-OSCReceiver::~OSCReceiver()
+OSCReceiver::~OSCReceiver() noexcept
 {
 }
 
@@ -112,7 +116,7 @@ template<class Iterator>
 float read_float(Iterator& first, const Iterator& last)
 {
 	float f;
-	ubyte_t *fptr=reinterpret_cast<ubyte_t*>(&f);
+	uint8_t *fptr=reinterpret_cast<uint8_t*>(&f);
 	ssize_t idx = 3;
 	while (first != last && idx>=0) {
 		fptr[idx--]=*first++;
@@ -124,7 +128,7 @@ template<class Iterator>
 int32_t read_int32(Iterator& first, const Iterator& last)
 {
 	int32_t i;
-	ubyte_t *fptr=reinterpret_cast<ubyte_t*>(&i);
+	uint8_t *fptr=reinterpret_cast<uint8_t*>(&i);
 	ssize_t idx = 3;
 	while (first != last && idx>=0) {
 		fptr[idx--]=*first++;
@@ -181,15 +185,26 @@ void OSCReceiver::process_data(Iterator& first, const Iterator& last)
 
 void OSCReceiver::run()
 {
-	IO_THREAD_PRE_RUN
-	socket_.reset(new asio::ASIOUDPSocket(log, get_this_ptr(),port_));
-	std::vector<ubyte_t> buffer(65536);
+//	IO_THREAD_PRE_RUN
+	//socket_.reset(new asio::ASIOUDPSocket(log, get_this_ptr(),port_));
+	log[log::info] << "Initializing socket of type '"<< socket_type_ << "'";
+	socket_ = core::DatagramSocketGenerator::get_instance().generate(socket_type_,log,"");
+	log[log::info] << "Binding socket";
+	if (!socket_->bind("",port_)) {
+		log[log::fatal] << "Failed to bind socket!";
+		request_end(core::yuri_exit_interrupted);
+		return;
+	}
+	log[log::info] << "Socket initialized";
+	std::vector<uint8_t> buffer(65536);
 	ssize_t read_bytes=0;
 	while(still_running()) {
 		if (!socket_->data_available()) {
-			sleep(10);
+			sleep(10_ms);
+			continue;
 		} else {
-			read_bytes = socket_->read(&buffer[0],buffer.size());
+			log[log::info] << "reading data";
+			read_bytes = socket_->receive_datagram(&buffer[0],buffer.size());
 			if (read_bytes > 0) {
 				log[log::info] << "Read " << read_bytes << " bytes";
 				auto first = buffer.begin();
@@ -198,12 +213,15 @@ void OSCReceiver::run()
 		}
 
 	}
-	IO_THREAD_POST_RUN
+//	IO_THREAD_POST_RUN
 	log[log::info] << "QUIT";
 }
 bool OSCReceiver::set_param(const core::Parameter& param)
 {
-	return core::BasicIOThread::set_param(param);
+	if (param.get_name() == "socket_type") {
+		socket_type_ = param.get<std::string>();
+	} else return core::IOThread::set_param(param);
+	return true;
 }
 
 } /* namespace osc_receiver */
