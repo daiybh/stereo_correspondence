04c5df1b0b69666ce59d9a5e892405c286aa99f8
Zdenek Travnicek <travnicek@iim.cz>
2013-10-23 15:45:12 +0200

    Converted uv_sdl to use generic ultragrid video sink interface

--- src/modules/uv_sdl/UVSdl.cpp
+++ src/modules/uv_sdl/UVSdl.cpp
@@ -9,8 +9,6 @@
 
 #include "UVSdl.h"
 #include "yuri/core/Module.h"
-#include "yuri/core/frame/RawVideoFrame.h"
-#include "yuri/ultragrid/YuriUltragrid.h"
 extern "C" {
 #include "video_display.h"
 #include "video_display/sdl.h"
@@ -32,82 +30,23 @@ core::Parameters UVSdl::configure()
 	return p;
 }
 
-namespace {
-class SDLHelper: public core::ThreadBase
-{
-public:
-	SDLHelper(const log::Log& log_, core::pwThreadBase parent, void* device):
-		ThreadBase(log_,parent,"SDLHelper"),device_(device)
-{}
-	~SDLHelper() noexcept {}
-private:
-	virtual void run() override
-{
-	log[log::info] << "SDL start";
-	try {
-		display_sdl_run(device_);
-	} catch (std::runtime_error& e) {
-		log[log::warning] << "UV exit: " << e.what();
-	}
-	log[log::info] << "SDL quit";
-}
-	void* device_;
-};
-}
-
-
 UVSdl::UVSdl(log::Log &log_, core::pwThreadBase parent, const core::Parameters &parameters):
-core::IOFilter(log_,parent,std::string("uv_sdl")),device_(nullptr),last_desc_{1,1,VIDEO_CODEC_NONE,1,PROGRESSIVE, 1}
+ultragrid::UVVideoSink(log_, parent, "uv_sdl", UV_SINK_DETAIL(sdl))
 {
 	IOTHREAD_INIT(parameters)
-	device_ = display_sdl_init(nullptr,0);
+	if (!init_sink("",0)) {
+		log[log::fatal] << "Failed to initialize SDL device";
+		throw exception::InitializationFailed("Failed to initialzie SDL device");
+	}
 }
 
 UVSdl::~UVSdl() noexcept
 {
-	try {
-		display_sdl_done(device_);
-	}
-	catch (std::runtime_error&) {}
 }
 
-void UVSdl::run()
-{
-	core::pThreadBase helper = make_shared<SDLHelper>(log,get_this_ptr(),device_);
-	spawn_thread(helper);
-	IOThread::run();
-}
-void UVSdl::child_ends_hook(core::pwThreadBase /*child*/, int /*code*/, size_t /*remaining_child_count*/)
-{
-	request_end(core::yuri_exit_interrupted);
-}
-core::pFrame UVSdl::do_simple_single_step(const core::pFrame& framex)
-{
-	core::pRawVideoFrame frame = dynamic_pointer_cast<core::RawVideoFrame>(framex);
-	if (frame) {
-		codec_t uv_fmt = ultragrid::yuri_to_uv(frame->get_format());
-		if (!uv_fmt) return {};
-		resolution_t res = frame->get_resolution();
-		video_desc desc {static_cast<unsigned int>(res.width), static_cast<unsigned int>(res.height), uv_fmt, 1,PROGRESSIVE, 1};
-		if (desc != last_desc_) {
-			display_sdl_reconfigure(device_, desc);
-			last_desc_ = desc;
-		}
-		//unique_ptr<video_frame, void(*)(video_frame*)> uv_frame (
-		unique_ptr<video_frame, function<void(video_frame*)>> uv_frame (
-				display_sdl_getf(device_),
-				[this](video_frame* uvf)mutable{display_sdl_putf(this->device_, uvf, false);});
-
-		if (!ultragrid::copy_to_uv_frame(frame, uv_frame.get())) {
-			log[log::warning ] << "Failed to convert frame";
-		}
-
-	}
-	return {};
-}
 bool UVSdl::set_param(const core::Parameter& param)
 {
-	return core::IOThread::set_param(param);
+	return ultragrid::UVVideoSink::set_param(param);
 }
 
 } /* namespace uv_sdl */
--- src/modules/uv_sdl/UVSdl.h
+++ src/modules/uv_sdl/UVSdl.h
@@ -10,12 +10,11 @@
 #ifndef UVSDL_H_
 #define UVSDL_H_
 
-#include "yuri/core/thread/IOFilter.h"
-#include "types.h"
+#include "yuri/ultragrid/UVVideoSink.h"
 namespace yuri {
 namespace uv_sdl {
 
-class UVSdl: public core::IOFilter
+class UVSdl: public ultragrid::UVVideoSink
 {
 public:
 	IOTHREAD_GENERATOR_DECLARATION
@@ -23,13 +22,9 @@ public:
 	UVSdl(log::Log &log_, core::pwThreadBase parent, const core::Parameters &parameters);
 	virtual ~UVSdl() noexcept;
 private:
-	
-	void run() override;
-	virtual core::pFrame do_simple_single_step(const core::pFrame& frame) override;
 	virtual bool set_param(const core::Parameter& param);
-	virtual void child_ends_hook(core::pwThreadBase child, int code, size_t remaining_child_count) override;
-	void* device_;
-	video_desc last_desc_;
+
+
 };
 
 } /* namespace uv_sdl */
