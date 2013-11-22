9c171eb11ab88bcf574c41c0f02f83c249413719
Zdenek Travnicek <travnicek@iim.cz>
2013-11-22 14:55:50 +0100

    Added module uv_uyvy

---
+++ src/modules/uv_uyvy/UVUyvy.cpp
@@ -0,0 +1,56 @@
+/*!
+ * @file 		UVUyvy.cpp
+ * @author 		Zdenek Travnicek <travnicek@iim.cz>
+ * @date		22.11.2013
+ * @copyright	CESNET, z.s.p.o, 2013
+ * 				Distributed under modified BSD Licence, details in file doc/LICENSE
+ *
+ */
+
+#include "UVUyvy.h"
+#include "yuri/core/Module.h"
+extern "C" {
+#include "video_compress.h"
+#include "video_compress/uyvy.h"
+#include "video_frame.h"
+}
+namespace yuri {
+namespace uv_uyvy {
+
+
+IOTHREAD_GENERATOR(UVUyvy)
+
+MODULE_REGISTRATION_BEGIN("uv_uyvy")
+		REGISTER_IOTHREAD("uv_uyvy",UVUyvy)
+MODULE_REGISTRATION_END()
+
+core::Parameters UVUyvy::configure()
+{
+	core::Parameters p = ultragrid::UVVideoCompress::configure();
+	p.set_description("UV UYVY");
+	return p;
+}
+
+
+UVUyvy::UVUyvy(const log::Log &log_, core::pwThreadBase parent, const core::Parameters &parameters):
+ultragrid::UVVideoCompress(log_,parent,"uv_uyvy", UV_COMPRESS_DETAIL(uyvy))
+{
+	IOTHREAD_INIT(parameters)
+	if(!init_compressor("")) {
+		log[log::fatal] << "Failed to create encoder";
+		throw exception::InitializationFailed("Failed to create UYVY encoder");
+	}
+}
+
+UVUyvy::~UVUyvy() noexcept
+{
+}
+
+
+bool UVUyvy::set_param(const core::Parameter& param)
+{
+	return ultragrid::UVVideoCompress::set_param(param);
+}
+
+} /* namespace uv_jpeg_compress */
+} /* namespace yuri */
---
+++ src/modules/uv_uyvy/UVUyvy.h
@@ -0,0 +1,32 @@
+/*!
+ * @file 		UVUyvy.h
+ * @author 		Zdenek Travnicek <travnicek@iim.cz>
+ * @date 		22.11.2013
+ * @copyright	CESNET, z.s.p.o, 2013
+ * 				Distributed under modified BSD Licence, details in file doc/LICENSE
+ *
+ */
+
+#ifndef UVUYVY_H_
+#define UVUYVY_H_
+
+#include "yuri/ultragrid/UVVideoCompress.h"
+
+namespace yuri {
+namespace uv_uyvy {
+
+class UVUyvy: public ultragrid::UVVideoCompress
+{
+public:
+	IOTHREAD_GENERATOR_DECLARATION
+	static core::Parameters configure();
+	UVUyvy(const log::Log &log_, core::pwThreadBase parent, const core::Parameters &parameters);
+	virtual ~UVUyvy() noexcept;
+private:
+	
+	virtual bool set_param(const core::Parameter& param);
+};
+
+} /* namespace uv_uyvy */
+} /* namespace yuri */
+#endif /* UVUYVY_H_ */
