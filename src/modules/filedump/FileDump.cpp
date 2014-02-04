2e1bec1659acf46c363c1a9f55ad35d5c7983db8
Zdenek Travnicek <travnicek@iim.cz>
2014-02-04 06:01:34 +0100

    Ported module filedump to yuri2.8

--- src/modules/filedump/FileDump.cpp
+++ src/modules/filedump/FileDump.cpp
@@ -12,82 +12,91 @@
 #include <sstream>
 #include <iomanip>
 #include "yuri/core/Module.h"
+#include "yuri/core/frame/RawVideoFrame.h"
+#include "yuri/core/frame/CompressedVideoFrame.h"
 namespace yuri
 {
 namespace dump
 {
 
-REGISTER("filedump",FileDump)
+MODULE_REGISTRATION_BEGIN("filedump")
+	REGISTER_IOTHREAD("filedump",FileDump)
+MODULE_REGISTRATION_END()
 
-core::pIOThread FileDump::generate(log::Log &_log,core::pwThreadBase parent, core::Parameters& parameters)
+IOTHREAD_GENERATOR(FileDump)
+
+core::Parameters FileDump::configure()
 {
-	shared_ptr<FileDump> dump;
-	try {
-		dump.reset(new FileDump(_log,parent,parameters));
-	}
-	catch (std::exception &e) {
-		throw exception::InitializationFailed(std::string("Filedump constuctor failed: ") + e.what());
-	}
-	return dump;
-}
-core::pParameters FileDump::configure()
-{
-	core::pParameters p (new core::Parameters());
-	p->set_description("Outputs frames to a file. It either dump all frames to one file of every frame to separate file. ");
-	(*p)["sequence"]["Number of digits in sequence number. Set to 0 to disable sequence and output all frames to one file."]=0;
-	(*p)["filename"]["Required parameter. Path of file to dump to"]=std::string();
-	(*p)["frame_limit"]["Maximal number of frames to dump. 0 for unlimited"]=0;
+	core::Parameters p = IOFilter::configure();
+	p.set_description("Outputs frames to a file. It either dump all frames to one file of every frame to separate file. ");
+	p["sequence"]["Number of digits in sequence number. Set to 0 to disable sequence and output all frames to one file."]=0;
+	p["filename"]["Required parameter. Path of file to dump to"]=std::string();
+	p["frame_limit"]["Maximal number of frames to dump. 0 for unlimited"]=0;
 	return p;
 }
 
-FileDump::FileDump(log::Log &log_,core::pwThreadBase parent,core::Parameters &parameters):
-	IOThread(log_,parent,1,0,"Dump"),
+FileDump::FileDump(log::Log &log_,core::pwThreadBase parent, const core::Parameters &parameters):
+	IOFilter(log_,parent,"Dump"),
 	dump_file(),filename(),seq_chars(0),seq_number(0),dumped_frames(0),
 	dump_limit(0)
 {
-	params.merge(*configure());
-	params.merge(parameters);
-	filename = params["filename"].get<std::string>();
-	seq_chars = params["sequence"].get<int>();
-	dump_limit = params["frame_limit"].get<yuri::size_t>();
+	IOTHREAD_INIT(parameters);
+	if (filename.empty()) throw exception::InitializationFailed("No filename specified");
 	if (!seq_chars)
 		dump_file.open(filename.c_str(), std::ios::binary | std::ios::out);
 }
 
-FileDump::~FileDump()
+FileDump::~FileDump() noexcept
 {
 	if (dump_file.is_open()) dump_file.close();
 }
 
-bool FileDump::step()
+core::pFrame FileDump::do_simple_single_step(const core::pFrame& frame)
 {
-	if (!in[0]) return true;
-	core::pBasicFrame f;
-	while ((f = in[0]->pop_frame()).get()) {
-		if (seq_chars) {
-			std::stringstream ss;
-			ss << filename.substr(0,filename.find_last_of('.'))
-				<< std::setfill('0') << std::setw(seq_chars) << seq_number++
-				<< filename.substr(filename.find_last_of('.'));
-			dump_file.open(ss.str().c_str(), std::ios::binary | std::ios::out);
+	if (seq_chars) {
+		std::stringstream ss;
+		const auto dot_index = filename.find_last_of('.');
+		if (dot_index != std::string::npos) {
+			ss << filename.substr(0,filename.find_last_of('.'));
+		} else {
+			ss << filename;
 		}
-		log[log::debug]<<"Dumping " << f->get_planes_count() << " planes" << std::endl;
+		ss << std::setfill('0') << std::setw(seq_chars) << seq_number++;
+		if (dot_index != std::string::npos) {
+			ss << filename.substr(filename.find_last_of('.'));
+		}
+		log[log::info] << "FIlename: " << ss.str() << ", idx " << dot_index;
+		dump_file.open(ss.str().c_str(), std::ios::binary | std::ios::out);
+	}
+	if (auto f = std::dynamic_pointer_cast<core::RawVideoFrame>(frame)) {
+		log[log::debug]<<"Dumping " << f->get_planes_count() << " planes";
 		for (yuri::size_t i=0; i<f->get_planes_count();++i) {
-			//log[log::debug]<<"Dumping plane " << i << ", size: " << (*f)[i].get_size() << std::endl;
-//			dump_file.write((const char *)(*f)[i].data.get(),(*f)[i].size);
 			dump_file.write(reinterpret_cast<const char *>(PLANE_RAW_DATA(f,0)),PLANE_SIZE(f,0));
 		}
-		if (seq_chars) {
-			dump_file.close();
-		}
-		// The comparison is evaluated FIRST in order to have dumped_frames counted even if dump_limit is zero
-		if (++dumped_frames >= dump_limit && dump_limit) {
-			log[log::debug] << "Maximal number of frames reached, quitting." << std::endl;
-			exitCode = YURI_EXIT_FINISHED;
-			request_end();
-			break;
-		}
+	} else if (auto f = std::dynamic_pointer_cast<core::CompressedVideoFrame>(frame)) {
+		dump_file.write(reinterpret_cast<const char *>(f->begin()),f->size());
 	}
+	if (seq_chars) {
+		dump_file.close();
+	}
+	// The comparison is evaluated FIRST in order to have dumped_frames counted even if dump_limit is zero
+	if (++dumped_frames >= dump_limit && dump_limit) {
+		log[log::debug] << "Maximal number of frames reached, quitting.";
+		request_end();
+	}
+
+	return {};
+}
+
+bool FileDump::set_param(const core::Parameter &param)
+{
+	if (param.get_name() == "filename") {
+		filename = param.get<std::string>();
+	} else if (param.get_name() == "sequence") {
+		seq_chars = param.get<int>();
+	} else if (param.get_name() == "frame_limit") {
+		dump_limit = param.get<size_t>();
+	} else return IOFilter::set_param(param);
 	return true;
 }
 
--- src/modules/filedump/FileDump.h
+++ src/modules/filedump/FileDump.h
@@ -10,7 +10,7 @@
 
 #ifndef FILEDUMP_H_
 #define FILEDUMP_H_
-#include "yuri/core/IOThread.h"
+#include "yuri/core/thread/IOFilter.h"
 #include <fstream>
 #include <string>
 
@@ -20,21 +20,22 @@ namespace dump
 {
 
 
-class FileDump: public core::IOThread
+class FileDump: public core::IOFilter
 {
 public:
-	FileDump(log::Log &log_, core::pwThreadBase parent, core::Parameters &parameters);
-	virtual ~FileDump();
-	static core::pIOThread generate(log::Log &_log,core::pwThreadBase parent, core::Parameters& parameters);
-	static core::pParameters configure();
-protected:
-	bool step();
-protected:
+	FileDump(log::Log &log_,core::pwThreadBase parent, const core::Parameters &parameters);
+	virtual ~FileDump() noexcept;
+	//static core::pIOThread generate(log::Log &_log,core::pwThreadBase parent, core::Parameters& parameters);
+	IOTHREAD_GENERATOR_DECLARATION
+	static core::Parameters configure();
+private:
+	virtual core::pFrame do_simple_single_step(const core::pFrame& frame) override;
+	virtual bool set_param(const core::Parameter &param) override;
 	std::ofstream dump_file;
 	std::string filename;
 
 	int seq_chars;
-	yuri::size_t seq_number, dumped_frames, dump_limit;
+	size_t seq_number, dumped_frames, dump_limit;
 
 
 };
