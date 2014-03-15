0268885d57c31ecdb0d69a226f84836ae589e8cb
Zdenek Travnicek <travnicek@iim.cz>
2014-03-15 21:51:59 +0100

    Added module select.

---
+++ src/modules/select/Select.cpp
@@ -0,0 +1,78 @@
+/*!
+ * @file 		Select.cpp
+ * @author 		<Your name>
+ * @date		15.03.2014
+ * @copyright	Institute of Intermedia, 2013
+ * 				Distributed BSD License
+ *
+ */
+
+#include "Select.h"
+#include "yuri/core/Module.h"
+#include "yuri/event/EventHelpers.h"
+
+namespace yuri {
+namespace select {
+
+
+IOTHREAD_GENERATOR(Select)
+
+MODULE_REGISTRATION_BEGIN("select")
+		REGISTER_IOTHREAD("select",Select)
+MODULE_REGISTRATION_END()
+
+core::Parameters Select::configure()
+{
+	core::Parameters p = base_type::configure();
+	p.set_description("Select");
+	p["index"]["input to pass through"]=0;
+	return p;
+}
+
+
+Select::Select(const log::Log &log_, core::pwThreadBase parent, const core::Parameters &parameters):
+core::IOThread(log_,parent,0,1,std::string("select")),BasicEventConsumer(log),index_(0)
+{
+	IOTHREAD_INIT(parameters)
+}
+
+Select::~Select() noexcept
+{
+}
+
+bool Select::step()
+{
+	process_events();
+	push_frame(0, pop_frame(index_));
+	return true;
+}
+bool Select::set_param(const core::Parameter& param)
+{
+	if (param.get_name() == "index") {
+		 index_ = param.get<position_t>();
+	} else return base_type::set_param(param);
+	return true;
+}
+
+void Select::do_connect_in(position_t pos, core::pPipe pipe)
+{
+	position_t inp = do_get_no_in_ports();
+	if (pos < 0) {
+		resize(inp+1,1);
+		pos = inp;
+	} else if (pos >= inp) {
+		resize(pos+1,1);
+	}
+	base_type::do_connect_in(pos, pipe);
+}
+
+bool Select::do_process_event(const std::string& event_name, const event::pBasicEvent& event)
+{
+	if (event_name == "index") {
+		index_ = event::lex_cast_value<position_t>(event);
+	}
+	return true;
+}
+
+} /* namespace select */
+} /* namespace yuri */
---
+++ src/modules/select/Select.h
@@ -0,0 +1,38 @@
+/*!
+ * @file 		Select.h
+ * @author 		<Your name>
+ * @date 		15.03.2014
+ * @copyright	Institute of Intermedia, 2013
+ * 				Distributed BSD License
+ *
+ */
+
+#ifndef SELECT_H_
+#define SELECT_H_
+
+#include "yuri/core/thread/MultiIOFilter.h"
+#include "yuri/event/BasicEventConsumer.h"
+
+namespace yuri {
+namespace select {
+
+class Select: public core::IOThread, public event::BasicEventConsumer
+{
+	using base_type = core::IOThread;
+public:
+	IOTHREAD_GENERATOR_DECLARATION
+	static core::Parameters configure();
+	Select(const log::Log &log_, core::pwThreadBase parent, const core::Parameters &parameters);
+	virtual ~Select() noexcept;
+private:
+	
+	virtual bool step() override;
+	virtual bool set_param(const core::Parameter& param) override;
+	virtual void do_connect_in(position_t, core::pPipe pipe) override;
+	virtual bool do_process_event(const std::string& event_name, const event::pBasicEvent& event) override;
+	position_t index_;
+};
+
+} /* namespace select */
+} /* namespace yuri */
+#endif /* SELECT_H_ */
