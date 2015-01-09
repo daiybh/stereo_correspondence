d492c2d7556a5910e33fa2d6ff07545f328339a3
Zdenek Travnicek <travnicek@iim.cz>
2015-01-09 08:52:02 +0100

    Removed use of yuri::map_list_of from V4l2Source.

--- src/modules/v4l2/V4l2Source.cpp
+++ src/modules/v4l2/V4l2Source.cpp
@@ -35,36 +35,33 @@ MODULE_REGISTRATION_END()
 namespace {
 	using namespace yuri::core::raw_format;
 	using namespace yuri::core::compressed_frame;
-	std::map<yuri::format_t, uint32_t> formats_map=
-			yuri::map_list_of<yuri::format_t, uint32_t>
-			(rgb24,		V4L2_PIX_FMT_RGB24)
-			(argb32, 	V4L2_PIX_FMT_RGB32)
-			(bgr24, 	V4L2_PIX_FMT_BGR24)
-			(bgra32, 	V4L2_PIX_FMT_BGR32)
-			(rgb15,		V4L2_PIX_FMT_RGB555)
-			(rgb16,		V4L2_PIX_FMT_RGB565)
-			(yuyv422, 	V4L2_PIX_FMT_YUYV)
-			(yvyu422, 	V4L2_PIX_FMT_YVYU)
-			(uyvy422, 	V4L2_PIX_FMT_UYVY)
-			(vyuy422, 	V4L2_PIX_FMT_VYUY)
-			//(yuv420p, V4L2_PIX_FMT_YUV420)
-//			(YURI_VIDEO_DV, V4L2_PIX_FMT_DV)
-//			(YURI_VIDEO_MJPEG, V4L2_PIX_FMT_MJPEG)
-//			(YURI_IMAGE_JPEG, V4L2_PIX_FMT_JPEG)
-			(bayer_bggr,V4L2_PIX_FMT_SBGGR8)
-			(bayer_rggb,V4L2_PIX_FMT_SRGGB8)
-			(bayer_grbg,V4L2_PIX_FMT_SGRBG8)
-			(bayer_gbrg,V4L2_PIX_FMT_SGBRG8)
+	std::map<yuri::format_t, uint32_t> formats_map= {
 
-			(mjpg, 		V4L2_PIX_FMT_MJPEG)
-			(jpeg, 		V4L2_PIX_FMT_JPEG)
+			{rgb24,		V4L2_PIX_FMT_RGB24},
+			{argb32, 	V4L2_PIX_FMT_RGB32},
+			{bgr24, 	V4L2_PIX_FMT_BGR24},
+			{bgra32, 	V4L2_PIX_FMT_BGR32},
+			{rgb15,		V4L2_PIX_FMT_RGB555},
+			{rgb16,		V4L2_PIX_FMT_RGB565},
+			{yuyv422, 	V4L2_PIX_FMT_YUYV},
+			{yvyu422, 	V4L2_PIX_FMT_YVYU},
+			{uyvy422, 	V4L2_PIX_FMT_UYVY},
+			{vyuy422, 	V4L2_PIX_FMT_VYUY},
+			//{yuv420p, V4L2_PIX_FMT_YUV420},
+//			{YURI_VIDEO_DV, V4L2_PIX_FMT_DV},
+			{bayer_bggr,V4L2_PIX_FMT_SBGGR8},
+			{bayer_rggb,V4L2_PIX_FMT_SRGGB8},
+			{bayer_grbg,V4L2_PIX_FMT_SGRBG8},
+			{bayer_gbrg,V4L2_PIX_FMT_SGBRG8},
 
-			;
+			{mjpg, 		V4L2_PIX_FMT_MJPEG},
+			{jpeg, 		V4L2_PIX_FMT_JPEG}};
 
 
-	std::map<std::string, uint32_t> special_formats=yuri::map_list_of<std::string, uint32_t>
-		("S920", V4L2_PIX_FMT_SN9C20X_I420)
-		("BA81", V4L2_PIX_FMT_SBGGR8);
+
+	std::map<std::string, uint32_t> special_formats = {
+		{"S920", V4L2_PIX_FMT_SN9C20X_I420},
+		{"BA81", V4L2_PIX_FMT_SBGGR8}};
 
 	/** Converts yuri::format_t to v4l2 format
 	 * \param fmt V4l2 pixel format
