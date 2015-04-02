821f6b07685d52d799804a9bc1cb4ea9fdc1f28a
Zdenek Travnicek <travnicek@iim.cz>
2015-04-02 14:43:47 +0000

    Added utils/wall_time

---
+++ src/yuri/core/utils/wall_time.h
@@ -0,0 +1,27 @@
+/*!
+ * @file 		wall_time.h
+ * @author 		Zdenek Travnicek <travnicek@iim.cz>
+ * @date 		2. 4. 2015
+ * @copyright	Institute of Intermedia, CTU in Prague, 2015
+ * 				Distributed under BSD Licence, details in file doc/LICENSE
+ *
+ */
+
+#ifndef SRC_YURI_CORE_UTILS_WALL_CLOCK_H_
+#define SRC_YURI_CORE_UTILS_WALL_CLOCK_H_
+#include "yuri/core/utils/platform.h"
+#include <ctime>
+namespace yuri {
+namespace core {
+namespace utils {
+
+std::tm get_current_local_time();
+
+
+}
+}
+}
+
+
+
+#endif /* SRC_YURI_CORE_UTILS_WALL_CLOCK_H_ */
