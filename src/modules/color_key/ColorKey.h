c45d01be4866f9eb85f0364eda59ffa39848c2b8
Zdenek Travnicek <travnicek@iim.cz>
2015-08-31 11:16:52 +0000

    Added missing override specifiers to color_key

--- src/modules/color_key/ColorKey.h
+++ src/modules/color_key/ColorKey.h
@@ -35,7 +35,7 @@ private:
 
 //	virtual bool step();
 	virtual core::pFrame do_special_single_step(core::pRawVideoFrame frame) override;
-	virtual bool set_param(const core::Parameter& param);
+	virtual bool set_param(const core::Parameter& param) override;
 	virtual bool do_process_event(const std::string& event_name, const event::pBasicEvent& event) override;
 
 	template<class kernel>
