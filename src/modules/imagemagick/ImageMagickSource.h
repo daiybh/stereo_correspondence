33c01fe67e1362d322cbacbcde4fa90ae9516890
Zdenek Travnicek <travnicek@iim.cz>
2015-08-31 11:18:19 +0000

    Added missing override specifiers to imagemagick_source

--- src/modules/imagemagick/ImageMagickSource.h
+++ src/modules/imagemagick/ImageMagickSource.h
@@ -26,7 +26,7 @@ public:
 private:
 	virtual core::pFrame do_convert_frame(core::pFrame input_frame, format_t target_format) override;
 	virtual core::pFrame do_special_single_step(core::pCompressedVideoFrame frame) override;
-	virtual bool set_param(const core::Parameter& param);
+	virtual bool set_param(const core::Parameter& param)  override;
 	yuri::format_t format_;
 };
 
