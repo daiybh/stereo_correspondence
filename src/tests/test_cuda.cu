f301fdfdbf90198416e95825c9613ffde8bb029f
Zdenek Travnicek <travnicek@iim.cz>
2016-03-08 13:59:17 +0000

    Added tests using uvector to test_cuda

--- src/tests/test_cuda.cu
+++ src/tests/test_cuda.cu
@@ -2,7 +2,7 @@
 #include "catch.hpp"
 #include "yuri/cuda/Cuda.h"
 #include <numeric>
-
+#include <algorithm>
 
 namespace yuri{
 
@@ -14,20 +14,31 @@ void iota(int *a)
 
 
 __global__
-void cpy(int *a, int* b)
+void cpy(const int *a, int* b)
 {
 	b[threadIdx.x] = a[threadIdx.x];
 }
+
+template<typename T>
+bool operator==(const std::vector<T>& lhs, const uvector<T>& rhs)
+{
+	return lhs.size() == rhs.size() &&
+			std::equal(lhs.begin(), lhs.end(), rhs.begin());
+}
+
+
 
 
 TEST_CASE("cuda") {
 	std::vector<int> a(10, 0);
 	std::vector<int> b(10, 0);
+	uvector<int> c(10);
 	std::vector<int> expected(10, 0);
 	std::iota(expected.begin(), expected.end(), 0);
 	
 	auto mem_a = cuda::cuda_alloc<int>(a.size());
 	auto mem_b = cuda::cuda_alloc<int>(b.size());
+	auto mem_c = cuda::cuda_alloc<int>(c.size());
 	dim3 dimBlock( a.size(), 1 );
 	dim3 dimGrid( 1, 1 );
 			
@@ -47,6 +58,14 @@ TEST_CASE("cuda") {
 		
 		REQUIRE(cuda::copy_to_cpu(b, mem_b));
 		REQUIRE(a == b);
+		
+		REQUIRE(cuda::copy_to_gpu(mem_c, c));
+				
+		cpy<<<dimGrid, dimBlock>>>(mem_b.get(), mem_c.get());
+		
+		REQUIRE(cuda::copy_to_cpu(c, mem_c));
+		
+		REQUIRE( a == c );
 	}
 	
 		
